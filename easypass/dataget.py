07de6abdab76a858639f4743f3f94b28ff891c99
Matija Osrecki <mosrecki@gmail.com>
2013-12-29 21:06:32 +0100

    Refactored util and data generation.

---
+++ easypass/dataget.py
@@ -0,0 +1,59 @@
+"""Module for generating n-gram input data."""
+
+import numpy as np
+
+import util
+
+
+def _create_trans_probs(layout_map, all_chars, dist_coef):
+    """Create transitive probability matrix between characters."""
+    trans_probs = dict()
+
+    for src_chr in all_chars:
+        probs = np.empty(len(all_chars))
+
+        for cix, dst_chr in enumerate(all_chars):
+            coor1, coor2 = layout_map[src_chr], layout_map[dst_chr]
+            if src_chr != dst_chr:
+                probs[cix] = np.linalg.norm(coor1 - coor2)
+            else:
+                probs[cix] = 0
+
+        probs = np.exp(-probs / dist_coef)
+        probs /= np.add.reduce(probs)
+
+        trans_probs[src_chr] = util.DiscreteRandom(probs, all_chars)
+
+    return trans_probs
+
+
+def _create_ngram(char, trans_probs, k_param):
+    ngram = str(char)
+    for idx in xrange(k_param - 1):
+        drv = trans_probs[trans_probs[-1]]
+        ngram += drv.random()
+
+
+def generate_dist(layout_map, all_chars, k_param=4, dist_coef=10):
+    """Generate input data using stochastic distance based method."""
+    trans_probs = _create_trans_probs(layout_map, all_chars, dist_coef)
+
+    dataset = []
+    for char in all_chars:
+        left_part = _create_ngram(char, trans_probs)
+        right_part = _create_ngram(char, trans_probs)
+        example = left_part[::-1] + right_part[1:]
+
+        start = np.random.randint(4)
+        dataset.append(example[start:start + k_param])
+
+    return dataset
+
+
+def main():
+    """Do nothing."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- easypass/util.py
+++ easypass/util.py
@@ -4,34 +4,22 @@ import numpy as np
 
 
 NORMAL_LAYOUT = ['1234567890-=',
-                 'qwertyuiop[]\\',
+                 'qwertyuiop[]',
                  'asdfghjkl;\'',
                  'zxcvbnm,./']
 
 SHIFT_LAYOUT = ['!@#$%^&*()_+',
-                 'QWERTYUIOP{}|',
-                 'ASDFGHJKL:"',
-                 'ZXCVBNM<>?']
+                'QWERTYUIOP{}',
+                'ASDFGHJKL:"',
+                'ZXCVBNM<>?']
 
 
-class DiscreteRandom(object):
-    """Discrete random generator."""
-
-    def __init__(self, values, probabilities):
-        self.probs = np.array(probabilities)
-        self.vals = values
-        self.probs = np.add.accumulate(self.probs)
-
-    def random(self):
-        """Return single random value according to it's probability."""
-        return self.vals[np.digitize(np.random.random_sample(1), self.probs)][0]
-
-
-def layout_mapping():
+def layout_mapping(spacing=1.2, row_offsets=None):
     """Return full mapping from char to coordinates (row, col, shift)."""
     layout_map = dict()
-    row_offsets = [0, 0.6, 1, 1.6]
-    spacing = 1.2
+
+    if not row_offsets:
+        row_offsets = [0, 0.6, 1, 1.6]
 
     for row_num in xrange(4):
         row_coor, col_coor = row_num * spacing, row_offsets[row_num]
@@ -39,47 +27,29 @@ def layout_mapping():
 
         assert len(nrow) == len(srow)
         for col_num in xrange(len(NORMAL_LAYOUT[row_num])):
-            layout_map[nrow[col_num]] = (row_coor, col_coor, 0)
-            layout_map[srow[col_num]] = (row_coor, col_coor, 1)
+            layout_map[nrow[col_num]] = np.array([row_coor, col_coor, 0])
+            layout_map[srow[col_num]] = np.array([row_coor, col_coor, 1])
             col_coor += spacing
 
     return layout_map
 
 
-def generate_dataset(layout_map, all_chars, dist_coef=10, shift_coef=5):
-    """Generate data given a layout map and a set of characters."""
-    # Calculate transition probabilities for each character.
-    trans_probs = dict()
-    for src_chr in all_chars:
-        probs = np.empty(len(all_chars))
-        for cix, dst_chr in enumerate(all_chars):
-            coor1, coor2 = layout_map[src_chr], layout_map[dst_chr]
-            probs[cix] =  np.sqrt(np.square(coor1[0] - coor2[0]) +
-                                  np.square(coor1[1] - coor2[1]) +
-                                  abs(coor1[2] - coor2[2]) * shift_coef)
+class DiscreteRandom(object):
+    """Discrete random generator."""
 
-        probs = np.exp(-probs / dist_coef)
-        probs /= np.add.reduce(probs)
-        trans_probs[src_chr] = DiscreteRandom(probs, all_chars)
+    def __init__(self, values, probabilities):
+        self.probs = np.array(probabilities)
+        self.probs = np.add.accumulate(self.probs)
+        self.vals = np.array(values)
 
-    # Generate dataset.
-    dataset = []
-    for char in all_chars:
-        example1, example2 = str(char), str(char)
-        for idx in xrange(3):
-            drv = trans_probs[example1[-1]]
-            example1 += drv.random()
+    def random(self):
+        """Return single random value according to it's probability."""
+        idx = np.digitize(np.random.random_sample(1), self.probs)
+        return self.vals[idx][0]
 
-            drv = trans_probs[example2[-1]]
-            example2 += drv.random()
-
-        example = example1[::-1] + example2[1:]
-        start = np.random.randint(4)
-        dataset.append(example[start:start+4])
-
-    return dataset
 
 def main():
+    """Do nothing."""
     pass
 
 
